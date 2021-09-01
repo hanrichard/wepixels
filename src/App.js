cd44f171214cad6cabeb32d3f2e8681ecfc97391
richardhan <hxy.314@gmail.com>
2021-09-01 22:09:59 +1000

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          We-pixels  
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          coming soon 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <footer className="App-footer"><span><b>Email: </b>wepixels.co@gmail.com </span><span><b>ABN: </b>56&nbsp;627&nbsp;118&nbsp;665 </span></footer>
     </div>
   );
 }
